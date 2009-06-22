87b3ff526547b6fea2e3f254f2b91f91f99facde
coExp <coco_experience@hotmail.com>
2009-06-22 16:35:18 +0200

    Nice graph with consumtion

--- print.rb
+++ print.rb
@@ -2,8 +2,51 @@ require 'rubygems'  # allows for the loading of gems
 require 'graphviz'  # this loads the ruby-graphviz gem
 
 
-def verif file
-	return true
+def verif f
+	@@con = false
+	@rel = false
+	@con_f = false
+	@rel_f = false
+	@name_f = false
+	begin
+		file = File.open f, 'r'
+	rescue
+		puts 'Error with file: ' + f
+		return false
+	end
+
+	file.each { |line|
+		if line.match 'CONS' and line[0] == 91
+			if @rel_f == false and @name_f == false 
+				@con_f = true 
+				@@con = true
+			else
+				return false
+			end
+		end
+		if line.match 'RELATION' and line[0] == 91
+			if @con_f == false and @name_f == false 
+				@rel_f = true 
+				@rel = true
+			else
+				return false
+			end
+		end
+		if line.match 'NAME' and line[0] == 91
+			if @rel_f == false and @con_f == false 
+				@name_f = true 
+			else
+				return false
+			end
+		end
+		if line.match 'END' and line[0] == 91
+			@name_f = false
+			@con_f = false
+			@rel_f = false
+		end
+	}
+	puts	"1: #{@@con} #{@rel} #{@con_f} #{@rel_f} #{@name_f}"
+	return @rel
 end
 
 def rename name
@@ -28,19 +71,50 @@ def graph f
 	g = GraphViz::new( "structs", "type" => "graph" )
 	g[:rankdir] = "LR"
 	g.edge[:arrowsize]= "0.5"
+	g.edge[:arrowhead]= "open"
 
-	file.each do |line|
-		@rel = false if line.match 'END' and line[0] != 35
-		if @rel 
+	@rel_f = false
+	file.each_line do |line|
+		@rel_f = false if line.match 'END' and line[0] == 91
+		if @rel_f and line[0] != 35 			
 			l = line.split ' '
-			g.add_edge(l[0].upcase, l[1].upcase ).label= l[2]
+			if @@consos.key? "#{l[0].upcase} -> #{l[1].upcase}"
+				label = @@consos.fetch("#{l[0].upcase} -> #{l[1].upcase}").to_s + "/" + l[2]
+			else
+				label = "0/" + l[2]
+			end
+			puts "1:#{} 2:#{} lab:#{label}!"
+			g.add_edge(l[0].upcase, l[1].upcase ).label= label
 		end
-		@rel = true if line.match 'RELATION' and line[0] != 35
+		@rel_f = true if line.match 'RELATION' and line[0] == 91
 	end
+	f = rename f 
+	puts "file: #{f}"
 	g.output( "output" => "png", :file => rename(f) )
 	file.close
 end
 
+def load_con f
+	file = File.open(f,'r')
+	@@consos = {}
+	@con = false
+	file.each do |line|
+		@con = false if line.match 'END' and line[0] == 91
+		if @con and line[0] != 35
+			l = line.split ' ' 
+			@@consos["#{l[0].upcase} -> #{l[1].upcase}"] = l[2]
+		end
+		@con = true if line.match 'CONS' and line[0] == 91
+	end
+	file.close
+	puts "2: #{@@consos} "
+end
+
 ARGV.each do |file|
-	graph(file) if verif(file)
+	if verif(file)
+		load_con(file) if @@con
+		graph(file)
+	else 
+		puts 'Error with file: ' + file
+	end
 end
